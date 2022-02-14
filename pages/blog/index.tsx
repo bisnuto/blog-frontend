3f9cd18f9194d18328ad887df60144fa85d66454
ALIVE20180411 <>
2022-02-14 16:30:17 +0900

    my first blog page

---
+++ pages/blog/index.tsx
@@ -0,0 +1,23 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../../styles/Blog.module.css'
+
+const Home: NextPage = () => {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Blog - Fernando Eiras</title>
+        <meta name="description" content="My first next js page" />
+        <link rel="icon" href="/images/favicon.ico" />
+      </Head>
+      <main>
+        <section className={styles.Intro}>
+          <p>Veraspera add astra.</p>
+        </section>
+      </main>
+    </div>
+  )
+}
+
+export default Home
