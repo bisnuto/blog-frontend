630c0a10c24c643cbfaea72f066c4965734eac96
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-03-21 11:07:37 +0100

    Add date object, code clean up and jsx order arrange, no styles

--- pages/blog/[id].tsx
+++ pages/blog/[id].tsx
@@ -1,9 +1,12 @@
 import * as React from "react";
 import type { NextPage } from "next";
-import { useRouter } from 'next/router'
+import { useRouter } from "next/router";
 import Head from "next/head";
 import Image from "next/image";
+// Project imports
 import styles from "@/styles/pages/BlogDetail.module.scss";
+
+// Types
 type TBlogDetail = {
     id: number;
     meta: {
@@ -15,17 +18,17 @@ type TBlogDetail = {
         seo_title: string;
         search_description: string;
         first_published_at: string;
-        alias_of: string|null;
+        alias_of: string | null;
         parent: {
             id: number;
             meta: {
                 type: string;
                 detail_url: string;
                 html_url: string;
-            },
+            };
             title: string;
-        }
-    },
+        };
+    };
     title: string;
     published_date: string;
     body: string;
@@ -35,37 +38,43 @@ type TBlogDetail = {
             type: string;
             detail_url: string;
             download_url: string;
-        },
+        };
         title: string;
-    }
-}
+    };
+};
+
+// Page component
 const BlogDetail: NextPage = () => {
     const router = useRouter();
     const [postData, setPostData] = React.useState<TBlogDetail | null>(null);
+
     React.useEffect(() => {
         let blogUrl = "";
-        async function fetchPostJSON(url:string) {
+        async function fetchPostJSON(url: string) {
             try {
                 const response = await fetch(url);
-                const post : TBlogDetail = await response.json();
-                
+                const post: TBlogDetail = await response.json();
+
                 setPostData(post);
             } catch (error) {
                 console.error(error);
                 console.log("here");
             }
-    
-          }
-        if(router.isReady){
-            const {id} = router.query;
+        }
+        if (router.isReady) {
+            const { id } = router.query;
             blogUrl = `http://127.0.0.1:8000/api/v2/cms/pages/${id}`;
             fetchPostJSON(blogUrl);
         }
-       
-          
-    }, []) 
-    const imgSrc = postData ? `http://127.0.0.1:8000${postData.feed_image.meta.download_url}`: "";
-    console.log(imgSrc);
+    }, [router]);
+
+    // State derived variables using JS ternary operator, JS6 template strings and new Date constructor
+    const imgSrc = postData
+        ? `http://127.0.0.1:8000${postData.feed_image.meta.download_url}`
+        : "";
+    const blogDate = postData ? new Date(postData.published_date) : null;
+
+    // returned JSX
     return (
         <div className={styles.container}>
             <Head>
@@ -75,27 +84,33 @@ const BlogDetail: NextPage = () => {
             </Head>
             <>
                 <section className="container">
-                    
-                    {postData? (
+                    {postData ? (
                         <div>
-                            <h1>{postData.title}</h1>
                             <div className={styles.BlogImgcont}>
-                            <Image
-                            src= {imgSrc}
-                            className={styles.BlogImg}
-                            layout="fill"
-                            objectFit="cover"
-                            alt="Autumn Sale Banner"
-                            objectPosition="center"
-                            />
+                                <Image
+                                    src={imgSrc}
+                                    className={styles.BlogImg}
+                                    layout="fill"
+                                    objectFit="cover"
+                                    alt="Autumn Sale Banner"
+                                    objectPosition="center"
+                                />
+                            </div>
+                            <h1>{postData.title}</h1>
+                            <div>
+                                {blogDate ? (
+                                    <p>{`${blogDate.getFullYear()}年${blogDate.getMonth()}月${blogDate.getDate()}日`}</p>
+                                ) : null}
                             </div>
                             <div>
-                                <div dangerouslySetInnerHTML={{__html: postData.body}} />
-                                <p>{postData.published_date}</p>
+                                <div
+                                    dangerouslySetInnerHTML={{
+                                        __html: postData.body,
+                                    }}
+                                />
                             </div>
                         </div>
                     ) : null}
-                    
                 </section>
             </>
         </div>
