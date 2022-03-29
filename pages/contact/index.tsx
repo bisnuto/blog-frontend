7d90bb8595c82db9a8969be0568d30f28b456bbf
ALIVE20180411 <>
2022-03-29 18:04:08 +0900

    Form state

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -8,9 +8,14 @@ const Contact: NextPage = () => {
     const [name,setName] = React.useState("")
     const [email,setEmail] = React.useState("")
     const [message,setMessage] = React.useState("")
+    const [status,setStatus] = React.useState({
+        state: "idle",
+        errors: [],
+    })
     const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
-
+        
         e.preventDefault();
+        setStatus({...status,state:"loading"})
         const formData = {
             name: name,
             email: email,
@@ -26,14 +31,61 @@ const Contact: NextPage = () => {
                 },
                 body: JSON.stringify(formData) // body data type must match "Content-Type" header
               });
+              if(!response.ok){
+                  throw new Error("Sorry error..")
+              }
               const responseJson = await response.json(); // parses JSON response into native JavaScript objects
-        } catch (error) {
-            
+              setMessage(""); 
+              setEmail("");
+              setName("");
+              setStatus({...status,state:"success"})
+              setTimeout(()=>{
+                setStatus({...status,state:"idle"})
+              },2000)
+            } catch (error) {
+                setStatus({...status,state:"error"})
+            console.error(error);
         }
         
         
     }
-    
+    let form = null
+    if(status.state === "idle"){
+        form = (
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="name">Name</label>
+                    <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="email">E-mail address</label>
+                    <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
+                </div>
+                <div>
+                    <label htmlFor="message">message</label>
+                    <input type="text" name="message" value={message} onChange={(e)=>setMessage(e.target.value)}/>
+                </div>
+                <button type="submit">Send</button>
+            </form>
+        ) 
+    }else if(status.state === "loading")
+    {
+        form = <p>Loading</p>
+    }else if(status.state === "success"){
+       form = <p>Your message was sent!</p>
+    }else{
+        form = (
+            <div>
+                <p>Message fail, plaese try again.</p>
+                <button onClick={()=>{
+                    setMessage(""); 
+                    setEmail("");
+                    setName("");
+                    setStatus({...status,state: "idle"})
+                    }}>Try again</button>
+            </div>            
+        )
+    }
     return (
         <>
             <Head>
@@ -44,21 +96,8 @@ const Contact: NextPage = () => {
             <>
                 <section className="container">
                     <h1>Contact Page</h1>
-                    <form onSubmit={handleSubmit}>
-                        <div>
-                            <label htmlFor="name">Name</label>
-                            <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
-                        </div>
-                        <div>
-                            <label htmlFor="email">E-mail address</label>
-                            <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                        </div>
-                        <div>
-                            <label htmlFor="message">message</label>
-                            <input type="text" name="message" value={message} onChange={(e)=>setMessage(e.target.value)}/>
-                        </div>
-                        <button type="submit">Send</button>
-                    </form>
+                    {form}
+                    
                 </section>
             </>
         </>
