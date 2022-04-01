0960da837415caedd5ec2c0036e966b55f2534cf
Gareth Macdonald <macdonald.gareth@gmail.com>
2022-04-01 13:34:24 +0200

    Fix up email field errors, clean up code and comments

--- pages/contact/index.tsx
+++ pages/contact/index.tsx
@@ -1,8 +1,19 @@
+// library imports
+import * as React from "react";
 import type { NextPage } from "next";
 import Head from "next/head";
 import Image from "next/image";
+// app imports
 import styles from "@/styles/pages/Contact.module.scss";
-import * as React from "react";
+
+/*
+ * Types
+ */
+type TContactErrors = {
+    name?: string[];
+    email?: string[];
+    message?: string[];
+};
 
 type TContactResponse = {
     data: {
@@ -10,112 +21,141 @@ type TContactResponse = {
         email: string;
         message: string;
     } | null;
-    errors: {
-        name?: string[];
-        email?: string[];
-        message?: string[];
-    } | null
+    errors: TContactErrors | null;
+};
 
-}
+type TStatus = {
+    state: "idle" | "loading" | "success" | "error";
+    errors: TContactErrors | null;
+};
+
+/*
+ * Contact Page component
+ */
 const Contact: NextPage = () => {
-    const [name,setName] = React.useState("")
-    const [email,setEmail] = React.useState("")
-    const [message,setMessage] = React.useState("")
-    const [status,setStatus] = React.useState({
+    // State for component here
+    const [name, setName] = React.useState("");
+    const [email, setEmail] = React.useState("");
+    const [message, setMessage] = React.useState("");
+    const [status, setStatus] = React.useState<TStatus>({
         state: "idle",
         errors: null,
-    })
-    const handleSubmit = async (e:React.FormEvent<HTMLFormElement>) => {
-        
+    });
+    // handle sumbit function
+    const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
         e.preventDefault();
-        setStatus({...status,state:"loading"})
+        setStatus({ ...status, state: "loading" });
         const formData = {
             name: name,
             email: email,
-            message: message
-        }
+            message: message,
+        };
         const url = "http://127.0.0.1:8000/api/v2/contact/";
         try {
             const response = await fetch(url, {
-                method: 'POST', // *GET, POST, PUT, DELETE, etc.
+                method: "POST", // *GET, POST, PUT, DELETE, etc.
                 headers: {
-                  'Content-Type': 'application/json'
-                  // 'Content-Type': 'application/x-www-form-urlencoded',
+                    "Content-Type": "application/json",
                 },
-                body: JSON.stringify(formData) // body data type must match "Content-Type" header
-              });
-              if(!response.ok){
-                  throw new Error("Sorry error..")
-              }
-              const responseJson:TContactResponse = await response.json(); // parses JSON response into native JavaScript objects
-              console.log(responseJson)
-              if(responseJson.errors){
-                const statusCopy = JSON.parse(JSON.stringify(status))
-                statusCopy.errors = responseJson.errors
-                setStatus(statusCopy)
-                setStatus({...status,state:"idle"})
-                console.log("here")
-
-              }else{
-                setMessage(""); 
+                body: JSON.stringify(formData), // body data type must match "Content-Type" header
+            });
+            if (!response.ok) {
+                // Deal with non HTTP status 200 errors here
+                throw new Error("Sorry error..");
+            }
+            const responseJson: TContactResponse = await response.json(); // parses JSON response into native JavaScript objects
+            console.log(responseJson);
+            if (responseJson.errors) {
+                // Deal with field errors here
+                const statusCopy = JSON.parse(JSON.stringify(status));
+                statusCopy.errors = responseJson.errors;
+                statusCopy.state = "idle";
+                setStatus(statusCopy);
+                console.log("here");
+            } else {
+                // Deal with successful submission here
+                setMessage("");
                 setEmail("");
                 setName("");
-                setStatus({...status,state:"success"})
-                setTimeout(()=>{
-                  setStatus({...status,state:"idle"})
-                },2000)
-              }
-                                setStatus({...status,state:"idle"})
-
-            } catch (error) {
-                setStatus({...status,state:"error"})
+                setStatus({ errors: null, state: "success" });
+                setTimeout(() => {
+                    setStatus({ errors: null, state: "idle" });
+                }, 2000);
+            }
+        } catch (error) {
+            // catch and deal with errors from (!response.ok) or any other network errors
+            setStatus({ ...status, state: "error" });
             console.error(error);
         }
-        
-        
-    }
-    let form = null
-    if(status.state === "idle"){
+    }; // end handleSubmit
+
+    //Create and set the value of the form variable here bases on status.state
+    let form = null;
+    if (status.state === "idle") {
+        console.log("status", status);
         form = (
             <form onSubmit={handleSubmit}>
                 <div>
                     <label htmlFor="name">Name</label>
-                    <input type="text" name="name" value={name} onChange={(e)=>setName(e.target.value)}/>
+                    <input
+                        type="text"
+                        name="name"
+                        value={name}
+                        onChange={(e) => setName(e.target.value)}
+                    />
                 </div>
                 <div>
                     <label htmlFor="email">E-mail address</label>
-                    <input type="email" name="email" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                    <ul>{status?.errors?.email ? status.errors.email.map((error:string) => {
-                        return <li key={error}>{error}</li>
-                    }
-                    ):null}</ul>
-                    
+                    <input
+                        type="email"
+                        name="email"
+                        value={email}
+                        onChange={(e) => setEmail(e.target.value)}
+                    />
+                    {status?.errors?.email ? (
+                        <ul>
+                            {status.errors.email.map((error: string) => {
+                                return <li key={error}>{error}</li>;
+                            })}
+                        </ul>
+                    ) : null}
                 </div>
                 <div>
                     <label htmlFor="message">message</label>
-                    <input type="text" name="message" value={message} onChange={(e)=>setMessage(e.target.value)}/>
+                    <input
+                        type="text"
+                        name="message"
+                        value={message}
+                        onChange={(e) => setMessage(e.target.value)}
+                    />
                 </div>
                 <button type="submit">Send</button>
             </form>
-        ) 
-    }else if(status.state === "loading")
-    {
-        form = <p>Loading</p>
-    }else if(status.state === "success"){
-       form = <p>Your message was sent!</p>
-    }else{
+        );
+    } else if (status.state === "loading") {
+        form = <p>Loading</p>;
+    } else if (status.state === "success") {
+        form = <p>Your message was sent!</p>;
+    } else {
         form = (
             <div>
                 <p>Message fail, plaese try again.</p>
-                <button onClick={()=>{
-                    setMessage(""); 
-                    setEmail("");
-                    setName("");
-                    setStatus({...status,state: "idle"})
-                    }}>Try again</button>
-            </div>            
-        )
+                <button
+                    onClick={() => {
+                        setMessage("");
+                        setEmail("");
+                        setName("");
+                        setStatus({ ...status, state: "idle" });
+                    }}
+                >
+                    Try again
+                </button>
+            </div>
+        );
     }
+    /*
+     * JSX return from here
+     */
     return (
         <>
             <Head>
@@ -127,11 +167,11 @@ const Contact: NextPage = () => {
                 <section className="container">
                     <h1>Contact Page</h1>
                     {form}
-                    
                 </section>
             </>
         </>
     );
-};
+}; //end Contact
 
+// exports here
 export default Contact;
